6aa9302493d9ba87ea68319d863e476307f83580
Angela Wu <35179663+AngelaWuGitHub@users.noreply.github.com>
2020-01-26 22:35:12 -0600

    tobytes fix

--- HW3/ibm_ubuntu/mqtt_subscriber_remote.py
+++ HW3/ibm_ubuntu/mqtt_subscriber_remote.py
@@ -21,16 +21,17 @@ def on_message(client, userdata, msg):
         # file = open("/mnt/mybucket/testfile-{}.txt".format(counter), "w")
         # file.write("Message received")
         # file.close()
-        buff = np.fromstring(msg.payload, np.uint8)
+        # if msg = png.tostring() is used in face_reg.py
+        # then use buff = np.fromstring(msg.payload, np.uint8)
+        # if msg = png.tobytes() is used in face_reg.py
+        # then use the following
+        buff = np.frombuffer(msg.payload, dtype=np.uint8)
         buff = buff.reshape(1, -1)
         img = cv2.imdecode(buff, cv2.IMREAD_COLOR)
-        cv2.imwrite("/mnt/mybucket/face-{}.jpg".format(counter), img)
+        cv2.imwrite("/mnt/mybucket/test-{}.jpg".format(counter), img)
         counter += 1
     except:
         print('Message not received')
-    # if msg.payload.decode() == "Hello world!":
-    #     print("Yes!")
-    #     client.disconnect()
 
 
 client = mqtt.Client()
