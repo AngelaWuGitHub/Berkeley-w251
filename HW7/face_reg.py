b57568f3febc7215b3234d6711ef271915222c55
AngelaWuGitHub <lling086@gmail.com>
2020-02-16 13:15:51 -0600

    HW7 - submit code

--- HW7/face_reg.py
+++ HW7/face_reg.py
@@ -1,16 +1,90 @@
 import cv2
 import paho.mqtt.client as mqtt
 
+import sys
+import os
+import urllib
+# tf.contrib doesn't exist in 2.0.
+import tensorflow.contrib.tensorrt as trt
+# Code below works with TF2
+# from tensorflow.python.compiler.tensorrt import trt_convert as trt
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import tensorflow as tf
+import numpy as np
+import time
+from tf_trt_models.detection import download_detection_model, build_detection_graph
+
 print(cv2.getBuildInformation())
+matplotlib.use('Agg')
 
 # 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
 cap = cv2.VideoCapture(1)
 
 print('Video device connection status: ', cap.isOpened())
+# Capture frame per second
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+print('-------------------------------------- fps: {}'.format(fps))
 
 # To set the resolution
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+
+# Download an SSD model for face detection
+# https://github.com/yeephycho/tensorflow-face-detection
+FROZEN_GRAPH_NAME = 'data/frozen_inference_graph_face.pb'
+
+# Load the frozen graph
+output_dir = ''
+frozen_graph = tf.GraphDef()
+with open(os.path.join(output_dir, FROZEN_GRAPH_NAME), 'rb') as f:
+    frozen_graph.ParseFromString(f.read())
+# https://github.com/NVIDIA-AI-IOT/tf_trt_models/blob/master/tf_trt_models/detection.py
+INPUT_NAME = 'image_tensor'
+BOXES_NAME = 'detection_boxes'
+CLASSES_NAME = 'detection_classes'
+SCORES_NAME = 'detection_scores'
+MASKS_NAME = 'detection_masks'
+NUM_DETECTIONS_NAME = 'num_detections'
+input_names = [INPUT_NAME]
+output_names = [BOXES_NAME, CLASSES_NAME, SCORES_NAME, NUM_DETECTIONS_NAME]
+
+# Optimize the frozen graph using TensorRT
+trt_graph = trt.create_inference_graph(
+    input_graph_def=frozen_graph,
+    outputs=output_names,
+    max_batch_size=1,
+    max_workspace_size_bytes=1 << 25,
+    precision_mode='FP16',
+    minimum_segment_size=50
+)
+
+# Create session and load graph
+tf_config = tf.ConfigProto()
+tf_config.gpu_options.allow_growth = True
+tf_sess = tf.Session(config=tf_config)
+# use this if you want to try on the optimized TensorRT graph
+# Note that this will take a while
+# tf.import_graph_def(trt_graph, name='')
+# use this if you want to try directly on the frozen TF graph
+# this is much faster
+tf.import_graph_def(frozen_graph, name='')
+tf_input = tf_sess.graph.get_tensor_by_name(input_names[0] + ':0')
+tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
+tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
+tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
+tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
+
+# OpenCV CascadeClassifier
+# Downloaded from 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+is_load = face_cascade.load('haarcascade_frontalface_default.xml')
+
+if is_load:
+    print('Load haarcascades successfully')
+else:
+    print('Fail to load haarcascades')
 
 # Arguments for the publisher
 LOCAL_MQTT_BROKER = "jetson_mqtt_broker"
@@ -46,30 +120,13 @@ client.on_disconnect = on_disconnect
 #   http://www.steves-internet-guide.com/loop-python-mqtt-client/
 client.loop_start()
 
-# find -name "haarcascades"
-# my_ubuntu4
-# face_cascade = cv2.CascadeClassifier('/root/opencv-4.2.0/data/haarcascades/haarcascade_frontalface_default.xml')
-# is_load = face_cascade.load('/root/opencv-4.2.0/data/haarcascades/haarcascade_frontalface_default.xml')
-
-# my_ubuntu1
-# ./root/opencv_build/opencv/data/haarcascades
-# ./usr/local/share/opencv4/haarcascades
-# face_cascade = cv2.CascadeClassifier('/root/opencv_build/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
-# is_load = face_cascade.load('/root/opencv_build/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
-
-# jetson_ubuntu
-# Downloaded from 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml'
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-is_load = face_cascade.load('haarcascade_frontalface_default.xml')
-
-if is_load:
-    print('Load haarcascades successfully')
-else:
-    print('Fail to load haarcascades')
-
+count = 0
 while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    # Only capture 1 face per second
+    # if count % (1 * fps) != 0:
+    #     continue
     print(frame.shape)
     # We don't use the color information, so might as well save space
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -80,25 +137,83 @@ while (True):
         frame_used = gray
     else:
         frame_used = frame
+
     # Face detection
+    print('======================================== OpenCV ====================================')
+    t0 = time.time()
     faces = face_cascade.detectMultiScale(frame_used, 1.3, 5)
+    t1 = time.time()
+    print('Runtime: %f seconds' % float(t1 - t0))
+
     if len(faces) > 0:
         print('---------------- Face(s) detected')
     else:
         print('No face detected')
 
     for (x, y, w, h) in faces:
-        # Draw a rectangle around the face
-        cv2.rectangle(frame_used, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        # cut out face from the frame..
-        face_detected = frame_used[y:y + h, x:x + w]
-        # cv2.imshow('Preview Face', face_detected)
+        print('rectangle', x, y, w, h)
+        # # Draw a rectangle around the face
+        # # cv2.rectangle(image, start_point, end_point, color, thickness)
+        # cv2.rectangle(frame_used, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # # cut out face from the frame..
+        # face_detected = frame_used[y:y + h, x:x + w]
+        # # cv2.imshow('Preview Face', face_detected)
+        # rc, png = cv2.imencode('.png', face_detected)
+        # if rc:
+        #     print('Encode successfully')
+        # msg = png.tobytes()
+        # # Reference: https://stackoverflow.com/questions/17967320/python-opencv-convert-image-to-byte-string
+        # # msg = png.tostring()
+        #
+        # # Publish a message
+        # if flag_connected == 1:
+        #     print('Publishing messages')
+        #     client.publish(LOCAL_MQTT_TOPIC, msg)
+        # else:
+        #     print('No connection to MQTT broker')
+
+    print('======================================== tf_trt_models ====================================')
+    # Reference: https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
+    image_resized = np.array(cv2.resize(frame_used, (300, 300), interpolation=cv2.INTER_AREA))
+
+    # Run network on image
+    t0 = time.time()
+    scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections],
+                                                         feed_dict={
+                                                             tf_input: image_resized[None, ...]
+                                                         })
+
+    boxes = boxes[0]  # index by 0 to remove batch dimension
+    scores = scores[0]
+    classes = classes[0]
+    num_detections = num_detections[0]
+    t1 = time.time()
+    print('Runtime: %f seconds' % float(t1 - t0))
+
+    print('Number of potential faces detected: ', num_detections)
+
+    # suppress boxes that are below the threshold..
+    DETECTION_THRESHOLD = 0.5
+    # if int(num_detections) == 0:
+    #     print('No face detected')
+
+    for i in range(int(num_detections)):
+        if scores[i] < DETECTION_THRESHOLD:
+            # print('Confidence low')
+            continue
+
+        print('---------------- Face(s) detected')
+        # scale box to image coordinates
+        box = boxes[i] * np.array([frame_used.shape[0], frame_used.shape[1], frame_used.shape[0], frame_used.shape[1]])
+        print('rectangle', box[1], box[0], box[3], box[2])
+        # display rectangle
+        # cv2.rectangle(image, start_point, end_point, color, thickness)
+        cv2.rectangle(frame_used, (int(box[1]), int(box[0])), (int(box[3]), int(box[2])), (255, 0, 0), 2)
+        face_detected = frame_used[int(box[0]):int(box[2]), int(box[1]):int(box[3])]
         rc, png = cv2.imencode('.png', face_detected)
         if rc:
             print('Encode successfully')
         msg = png.tobytes()
-        # Reference: https://stackoverflow.com/questions/17967320/python-opencv-convert-image-to-byte-string
-        # msg = png.tostring()
 
         # Publish a message
         if flag_connected == 1:
@@ -107,11 +222,16 @@ while (True):
         else:
             print('No connection to MQTT broker')
 
+    count += 1
     # Waits for a user input to quit the application
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 client.disconnect()
+
+# close tf session
+tf_sess.close()
+
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
